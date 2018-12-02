79a68429f94409edaa68120b0018698f32b8d43d
Franclin Cabral <fcmo@cin.ufpe.br>
2018-12-02 18:04:59 -0300

    Project

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+var axios = require("axios");
+var onoff = require('onoff');
 var rc522 = require("rc522");
 
-console.log("Pronto, aguardar tags");
+var Gpio = onoff.Gpio,
 
+ledA = new Gpio(20, "out");
+ledV = new Gpio(16, "out");
+
+console.log("Pronto, aguardar tags");
+const baseAPI = "http://ec2-54-69-43-225.us-west-2.compute.amazonaws.com:8080/api/card/";
+const mockAPI = "https://httpbin.org/ip";
+const localAPI = "http://10.0.81.41:8080/api/card/";
 
 rc522((rfidSerialNumber) => {
-	console.log("Reading tag");
-	console.log(rfidSerialNumber);
+	const params = {
+		"sectionCode": "D-42",
+	}
+
+	// console.log(rfidSerialNumber);
+	axios.put(baseAPI + rfidSerialNumber, params)
+		.then((_) => {
+			ledV.write(0, () => { console.log("Led vermelho desligado") });
+			ledA.write(1, () => { 
+				setTimeout(() => ledA.writeSync(0), 3000);
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+			ledV.write(1, () => { 
+				setTimeout(() => ledA.writeSync(0), 3000);
+			});
+			ledA.write(0, () => { console.log("Led amarelo desligado") });
+		});
 });
 
 
--- networking.js
+++ networking.js
@@ -1,23 +1,34 @@
-
-const getParams = {
-    method: HTTPMethod.GET,
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-};
+var axios = require("axios").default;
 
 
-export default class Networking {
+// const getParams = {
+//     method: HTTPMethod.GET,
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     }
+// };
+
+
+module.exports = class Networking {
     
-    async fetchFromAPI(url, parameters = getParams){        
-        return fetch(url, parameters)
+    async get(url) {
+        return axios.get(url);
+    }
+
+    async post(url, parameters) {
+        return axios.post(url, parameters)
             .then(this.handleErrors);
     }
 
+    // async fetchFromAPI(url, parameters = {}){        
+    //     return fetch(url, parameters)
+    //         .then(this.handleErrors);
+    // }
+
     handleErrors(response){
         if (!response.ok) {
-            throw Error(response.status);
+            throw Error(response);
         }
         return response.json();
     }
